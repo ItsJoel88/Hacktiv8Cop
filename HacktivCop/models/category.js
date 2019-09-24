d92409f4bc9ac81565de2b56997020e39aa4b3a2
ItsJoel88 <imanuelnjodi@gmail.com>
2019-09-24 20:32:06 +0700

    first commit

--- HacktivCop/models/category.js
+++ HacktivCop/models/category.js
@@ -1,10 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Category = sequelize.define('Category', {
-    name: DataTypes.STRING
-  }, {});
+  const Model = sequelize.Sequelize.Model
+  class Category extends Model {}
+  Category.init(
+    {
+      name: DataTypes.STRING
+    }, {
+      sequelize
+    }
+  )
   Category.associate = function(models) {
     // associations can be defined here
+    Category.belongsToMany(models.Police,{through: models.CategoryPolice})
   };
   return Category;
 };
